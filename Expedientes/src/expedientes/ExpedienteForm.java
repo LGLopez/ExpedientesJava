9c6b2c438be890bb8e4c5ab78af6243381cfc499
LGLopez <DARKdestruktion@gmail.com>
2019-11-24 11:48:33 -0600

    El arbol funcionando a la perfeccion

--- Expedientes/src/expedientes/ExpedienteForm.java
+++ Expedientes/src/expedientes/ExpedienteForm.java
@@ -20,6 +20,9 @@ public class ExpedienteForm extends javax.swing.JFrame {
     String paraExpediente = null;
     ArrayList paraArbol = new ArrayList<File>();
     ArrayList probarMilisegundos = new ArrayList<Long>();
+    BinaryTree arbolExpedientes;
+    ArrayList paraNombre = new ArrayList<String>();
+    ArrayList paraResto = new ArrayList<String>();
     
     public ExpedienteForm() {
         initComponents();
@@ -83,15 +86,52 @@ public class ExpedienteForm extends javax.swing.JFrame {
         List<String> fileList = Files.readAllLines(filePath);
         
         String mostrar = fileList.get(0);
+        String restoExpediente = fileList.get(1);
         for(int i=1; i<fileList.size()-1; i++){
+            if(i>1){
+                restoExpediente += " ";
+                restoExpediente += fileList.get(i);
+            }
+                
             mostrar += " ";
             mostrar += fileList.get(i);
         }
         
         System.out.println(" " + mostrar);
-        
+        paraNombre.add(fileList.get(0));
+        paraResto.add(restoExpediente);
         paraArbol.add(prueba);
         probarMilisegundos.add(attr.creationTime().toMillis());
+    }
+    
+    public void saveToTree(){
+        int toRemove = 0;
+        
+        if(!probarMilisegundos.isEmpty()){
+            arbolExpedientes = new BinaryTree();
+            Long menor = (Long) probarMilisegundos.get(0);
+            while(!probarMilisegundos.isEmpty()){
+                for(int i=0; i<probarMilisegundos.size(); i++){
+                    toRemove=0;
+                    if((Long) probarMilisegundos.get(i) < menor){
+                        menor = (Long)probarMilisegundos.get(i);
+                        toRemove = i;
+                    }
+
+                }
+                if(!probarMilisegundos.isEmpty()){
+                    arbolExpedientes.addNode((Long) probarMilisegundos.get(toRemove), paraNombre.get(toRemove).toString(), paraResto.get(toRemove).toString());
+                    probarMilisegundos.remove(toRemove);
+                    paraNombre.remove(toRemove);
+                    paraResto.remove(toRemove);
+                }
+            }
+        }
+        else{
+            System.err.println("No hay nada para agregar al arbol.");
+        }
+
+
     }
     /*
     public void load() {
@@ -140,6 +180,9 @@ public class ExpedienteForm extends javax.swing.JFrame {
         txtExpediente = new javax.swing.JTextArea();
         toDebug = new javax.swing.JButton();
         toCheckArrayList = new javax.swing.JButton();
+        btnArbol = new javax.swing.JButton();
+        btnVerArbol = new javax.swing.JButton();
+        btnBuscar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -179,28 +222,58 @@ public class ExpedienteForm extends javax.swing.JFrame {
             }
         });
 
+        btnArbol.setText("Crear Arbol");
+        btnArbol.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnArbolActionPerformed(evt);
+            }
+        });
+
+        btnVerArbol.setText("Ver Arbol");
+        btnVerArbol.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnVerArbolActionPerformed(evt);
+            }
+        });
+
+        btnBuscar.setText("Buscar");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jLabel3)
-                    .addComponent(jLabel1)
-                    .addComponent(jLabel2))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(txtNombre)
-                    .addComponent(txtEdad, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 277, Short.MAX_VALUE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 51, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(btnRecargar)
-                    .addComponent(btnAgregar)
-                    .addComponent(toDebug)
-                    .addComponent(toCheckArrayList))
-                .addGap(28, 28, 28))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(toDebug)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(toCheckArrayList)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(btnArbol)
+                        .addGap(18, 18, 18)
+                        .addComponent(btnVerArbol)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(0, 0, Short.MAX_VALUE)
+                                .addComponent(btnBuscar))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(jLabel3)
+                                    .addComponent(jLabel1)
+                                    .addComponent(jLabel2))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(txtNombre)
+                                    .addComponent(txtEdad, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 277, Short.MAX_VALUE))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 55, Short.MAX_VALUE)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(btnRecargar)
+                                    .addComponent(btnAgregar))))
+                        .addGap(66, 66, 66))))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -220,15 +293,19 @@ public class ExpedienteForm extends javax.swing.JFrame {
                         .addGap(18, 18, 18)
                         .addComponent(btnRecargar)
                         .addGap(18, 18, 18)
-                        .addComponent(toDebug)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(toCheckArrayList))
+                        .addComponent(btnBuscar))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(18, 18, 18)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel3)
                             .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 134, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addContainerGap(40, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 22, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnArbol)
+                    .addComponent(btnVerArbol)
+                    .addComponent(toDebug)
+                    .addComponent(toCheckArrayList))
+                .addContainerGap())
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -281,6 +358,32 @@ public class ExpedienteForm extends javax.swing.JFrame {
         
     }//GEN-LAST:event_toCheckArrayListActionPerformed
 
+    private void btnArbolActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnArbolActionPerformed
+        saveToTree();
+    }//GEN-LAST:event_btnArbolActionPerformed
+
+    private void btnVerArbolActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVerArbolActionPerformed
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("In order:");
+        arbolExpedientes.inOrderTraverseTree(arbolExpedientes.root);
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("Pre order:");
+        
+        arbolExpedientes.preorderTraverseTree(arbolExpedientes.root);
+        
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("Post order:");
+        
+        arbolExpedientes.postOrderTraverseTree(arbolExpedientes.root);
+    }//GEN-LAST:event_btnVerArbolActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -318,7 +421,10 @@ public class ExpedienteForm extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnAgregar;
+    private javax.swing.JButton btnArbol;
+    private javax.swing.JButton btnBuscar;
     private javax.swing.JButton btnRecargar;
+    private javax.swing.JButton btnVerArbol;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
